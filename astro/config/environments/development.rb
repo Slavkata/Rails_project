0b823049eef5ee7ae7e8ef37f2d5ab2a101dbb65
Slavkata <slavkirilov00@gmail.com>
2017-04-18 23:31:57 +0300

    fixing some small issues

--- astro/config/environments/development.rb
+++ astro/config/environments/development.rb
@@ -63,9 +63,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  
+
   config.assets.raise_runtime_errors = true
-  Rails.application.config.assets.precompile += %w( games.js )
+  #Rails.application.config.assets.precompile += %w( games.js )
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
